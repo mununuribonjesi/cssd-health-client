0c52a9deff1b9c97fe27b2662e15abcb156c717e
Munibanks <mbonjesi@gmail.com>
2020-03-02 02:33:13 +0000

    //code cleaning

--- src/Contexts/authContext.js
+++ src/Contexts/authContext.js
@@ -1,36 +1,35 @@
 import React, { createContext, Component } from 'react';
-import axios from'axios';
-//creating a new a context
+import axios from 'axios';
+
+//creating Auth context
 
 export const AuthContext = createContext();
 
 class AuthenticationProvider extends Component {
-   
-   constructor(props)
-   {
-       super(props)
 
-    this.state = {
-        isAuthenticated: false,
-        username:'',
-        password:''
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isAuthenticated: false,
+            username: '',
+            password: ''
+        }
+
+        this.submitForm = this.submitForm.bind(this);
+        this.onChange = this.onChange.bind(this)
     }
 
-    this.submitForm = this.submitForm.bind(this);
-    this.onChange = this.onChange.bind(this)
-
-}
+    //UserAuthentication
 
     async login() {
-
-        const response =
-            await axios.post('https://shu-helth-uat.azurewebsites.net/api/login', {
-                'username': this.state.username,
-                'password': this.state.password
-            });
+        const response = await axios.post('https://shu-helth-uat.azurewebsites.net/api/login', {
+            'username': this.state.username,
+            'password': this.state.password
+        });
 
         if (response.status === 200) {
-            this.setState({isAuthenticated:false})
+            this.setState({ isAuthenticated: true })
             localStorage.setItem('token', response.data.token);
         }
     }
@@ -41,16 +40,17 @@ class AuthenticationProvider extends Component {
         })
     }
 
-    submitForm = (e) =>
-    {
-      e.preventDefault();
-      this.login();
+    submitForm = (e) => {
+        e.preventDefault();
+        this.login();
     }
-    
+
     render() {
         return (
-            <AuthContext.Provider value={{...this.state,
-                onChange:this.onChange,submitForm:this.submitForm}}>
+            <AuthContext.Provider value={{
+                ...this.state,
+                onChange: this.onChange, submitForm: this.submitForm
+            }}>
                 {this.props.children}
             </AuthContext.Provider>
         )
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
 import './nav.css';
 
-export class NavBar extends Component
-{
-  render () {
+export class NavBar extends Component {
+  render() {
     return (
       <nav className="navbar is-primary" role="navigation">
         <div className="container">
           <div className="navbar-brand">
-
-              <a href="/" className="navbar-item">
-                HELTH
+            <a href="/" className="navbar-item">
+              HELTH
               </a>
-     
+
           </div>
         </div>
 
         <div className="navbar-start">
           <a href='patientScreen' className="navbar-item">
-              Patients      
+            Patients
           </a>
 
-          <a href='patientRegistration'className="navbar-item">
-              Register      
+          <a href='patientRegistration' className="navbar-item">
+            Register
           </a>
         </div>
-      </nav> 
+      </nav>
     );
   }
 }
